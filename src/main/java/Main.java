8d35847c4bdccd00faa30b61b7df11cb21f4530f
Stark <olya.vigidna@gmail.com>
2019-04-24 00:41:35 +0300

    working connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import models.Degree;
 import models.Department;
 import models.Lector;
 import org.hibernate.Session;
@@ -14,10 +15,28 @@ public class Main {
                 .buildSessionFactory();
 
         Session session = sessionFactory.openSession();
-        session.beginTransaction();
 
+        Helper helper = new Helper(session);
+        helper.menu();
 
-        session.getTransaction().commit();
+//        session.beginTransaction();
+//
+//        Department department = new Department();
+//        Lector lector = new Lector();
+//        Lector lector2 = new Lector();
+//        department.setDepartmentName("Tormund");
+//        lector.setLectorDegree(Degree.PROFESSOR);
+//        lector.setLectorSalary(2340);
+//        lector2.setLectorDegree(Degree.ASSOCIATE_PROFESSOR);
+//        lector2.setLectorSalary(2500);
+//        lector2.setLectorName("Name");
+//        lector2.setLectorSurname("Surname");
+//        department.getLectors().add(lector);
+//        department.getLectors().add(lector2);
+//        department.setHeadOfDepartment(lector2);
+//        session.save(department);
+
+//        session.getTransaction().commit();
         session.close();
         sessionFactory.close();
     }
